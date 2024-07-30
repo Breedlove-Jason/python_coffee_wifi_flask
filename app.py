7c7699c2980df5348d600930af2b111928ef3799
Jason Breedlove <j@jasonbreedlove.com>
2024-07-29 18:48:52 -0700

    fix imports

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, flash
 import os
-from flask_bootstrap5 import Bootstrap
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
 from form import CafeForm
