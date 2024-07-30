0e3a92a38aa757a4f46dd6c9a107b2fd74e1d14f
Jason Breedlove <j@jasonbreedlove.com>
2024-07-29 18:37:13 -0700

    update requirements.txt

--- app.py
+++ app.py
@@ -54,6 +54,6 @@ def add_cafe():
     return render_template("add.html", form=form)
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
